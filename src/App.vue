0106265e6d9af17f09db63798b32714cf49cf061
Icer Liang <liangshuang@sutu.tech>
2024-12-02 16:46:31 +0800

    initialize page

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="news-container">
+    <div v-for="(card, index) in displayedCards" :key="index" class="news-card">
+      <div class="card-header">
+        <div class="github-title">
+          <div class="title-icon">
+            <span class="github-icon">{ }</span>
+            GitHub <span class="highlight">趋势</span>
+          </div>
+          <div class="subtitle-text">
+            GitHub 今天有什么热门项目🔥
+          </div>
+        </div>
+        <div class="date">
+          <span class="month">{{ card.month }}月</span>
+          <span class="day">{{ card.day }}</span>
+        </div>
+      </div>
+      <div class="card-subtitle">
+      </div>
+      <div class="projects">
+        <div v-for="(project, pIndex) in card.projects" :key="pIndex" class="project-item">
+          <div class="project-icon">
+            <span v-if="!project.iconUrl">{{ project.icon }}</span>
+            <img v-else :src="project.iconUrl" :alt="project.name">
+          </div>
+          <div class="project-content">
+            <h3 class="project-name">{{ project.name }}</h3>
+            <a :href="project.url" class="project-url">{{ project.url }}</a>
+            <p class="project-description">{{ project.description }}</p>
+            <div class="project-stats">
+              <span class="lang">{{ project.language }}</span>
+              <span class="stars">⭐ {{ project.stars }}</span>
+              <span class="forks">🔱 {{ project.forks }}</span>
+              <span class="trending">⭐ +{{ project.trending }}</span>
+            </div>
+            <p class="project-summary">{{ project.summary }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="card-footer">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { newsData } from './data'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      cards: newsData
+    }
+  },
+  computed: {
+    displayedCards() {
+      return this.cards
+    }
+  }
+}
+</script>
+
+<style>
+@import './style.css';
+</style>
