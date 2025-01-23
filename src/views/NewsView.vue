4b69fe1acb5c9ff5117e4bcce8a65832a0a0150e
Icer Liang <liangshuang@sutu.tech>
2025-01-23 13:38:32 +0000

    when summary is undefined, show something not blank

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -58,7 +58,7 @@
                 {{ tag }}
               </span>
             </div>
-            <p class="project-summary">{{ project.summary.replaceAll('\{\{name\}\}','') }}</p>
+            <p class="project-summary" v-if="project.summary">{{ project.summary.replaceAll('\{\{name\}\}','') }}</p>
           </div>
         </div>
       </div>
